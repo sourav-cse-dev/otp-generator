0070a7fe8beaa516bad506b5db5e8d90211e871f
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-01-15 13:03:10 +0600

    some logic implemented

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+function tackleOTPBoxes() {
+  const boxes = document.getElementById("otp-box-list-id");
+  boxes.addEventListener("input", function (event) {
+    const target = event.target;
+    const value = target.value;
+    if (isNaN(value)) {
+      target.value = "";
+      return;
+    }
+
+    const nextElement = target.nextElementSibling;
+    if (nextElement) {
+      nextElement.focus();
+    }
+  });
+}
+
+function generateOTP() {
+  const generatedOTP = Math.floor(1000 + Math.random() * 9000);
+  const otpElement = document.getElementById("generated-otp-id");
+  otpElement.innerText = `Your OTP: ${generatedOTP}`;
+}
+
 function init() {
   console.log("JavaScript initialization done!!!");
+  tackleOTPBoxes();
+  setTimeout(generateOTP, 2000);
+  validateOTP();
 }
 
 init();
